7028b710151ac32b7f34c81c488c12efae2ecbda
Yusuf <kaymazyusuf88@gmail.com>
2024-05-04 01:01:30 +0300

    animation_changed

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var sections = document.querySelectorAll(".fade-in-section");
-
-  function fadeInSections() {
-    sections.forEach(function (section) {
-      var sectionPosition = section.getBoundingClientRect().top;
-      var screenPosition = window.innerHeight / 1.3;
-
-      if (sectionPosition < screenPosition) {
-        section.classList.add("fade-in");
-      } else {
-        section.classList.remove("fade-in");
-      }
-    });
-  }
-
-  // Tüm bölümleri sayfa yüklendiğinde görünür yap
-  sections.forEach(function (section) {
-    section.classList.add("fade-in");
+document.addEventListener('DOMContentLoaded', function() {
+    var sections = document.querySelectorAll('.fade-in-section');
+    var animationDoneFlags = Array.from({ length: sections.length }, () => false);
+  
+    function fadeInSections() {
+      sections.forEach(function(section, index) {
+        var sectionPosition = section.getBoundingClientRect().top;
+        var screenPosition = window.innerHeight / 1.3;
+  
+        // Eğer animasyon daha önce çalışmamışsa ve içeriği ekrana getirecek kadar aşağıya scroll edildiyse,
+        // section'a fade-in sınıfını ekle ve bayrağı true yap
+        if (!animationDoneFlags[index] && sectionPosition < screenPosition) {
+          section.classList.add('fade-in');
+          animationDoneFlags[index] = true;
+        }
+      });
+    }
+  
+    // Tüm bölümleri sayfa yüklendiğinde görünür yap
+    fadeInSections();
+  
+    // Scroll olayını dinle
+    window.addEventListener('scroll', fadeInSections);
   });
-
-  // Scroll olayını dinle
-  window.addEventListener("scroll", fadeInSections);
-});
+  
