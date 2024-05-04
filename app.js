1fa0d96b6297cc8fee4963a5a22b17e48b7737f7
Yusuf <kaymazyusuf88@gmail.com>
2024-05-04 22:47:37 +0300

    font

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var sections = document.querySelectorAll('.fade-in-section');
-    var animationDoneFlags = Array.from({ length: sections.length }, () => false);
+// document.addEventListener('DOMContentLoaded', function() {
+//     var sections = document.querySelectorAll('.fade-in-section');
+//     var animationDoneFlags = Array.from({ length: sections.length }, () => false);
   
-    function fadeInSections() {
-      sections.forEach(function(section, index) {
-        var sectionPosition = section.getBoundingClientRect().top;
-        var screenPosition = window.innerHeight / 1.3;
+//     function fadeInSections() {
+//       sections.forEach(function(section, index) {
+//         var sectionPosition = section.getBoundingClientRect().top;
+//         var screenPosition = window.innerHeight / 1.3;
   
-        // Eğer animasyon daha önce çalışmamışsa ve içeriği ekrana getirecek kadar aşağıya scroll edildiyse,
-        // section'a fade-in sınıfını ekle ve bayrağı true yap
-        if (!animationDoneFlags[index] && sectionPosition < screenPosition) {
-          section.classList.add('fade-in');
-          animationDoneFlags[index] = true;
-        }
-      });
-    }
+//         // Eğer animasyon daha önce çalışmamışsa ve içeriği ekrana getirecek kadar aşağıya scroll edildiyse,
+//         // section'a fade-in sınıfını ekle ve bayrağı true yap
+//         if (!animationDoneFlags[index] && sectionPosition < screenPosition) {
+//           section.classList.add('fade-in');
+//           animationDoneFlags[index] = true;
+//         }
+//       });
+//     }
   
-    // Tüm bölümleri sayfa yüklendiğinde görünür yap
-    fadeInSections();
+//     // Tüm bölümleri sayfa yüklendiğinde görünür yap
+//     fadeInSections();
   
-    // Scroll olayını dinle
-    window.addEventListener('scroll', fadeInSections);
-  });
+//     // Scroll olayını dinle
+//     window.addEventListener('scroll', fadeInSections);
+//   });
   
